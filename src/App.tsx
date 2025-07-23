e6b5efb6a97a3e43641fb2ca55d137e355e5e202
Typhoon1612 <119401052+Typhoon1612@users.noreply.github.com>
2025-07-23 11:27:17 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Button from './components/button.tsx'
+
+interface Prop {
+
+}
+
+function App() {
+
+  return (
+    <>
+      <Button />
+    </>
+  )
+}
+
+export default App
