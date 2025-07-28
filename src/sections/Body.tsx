bd1d95eef5b1df4837e2ff95f9c54350efa7ebe8
Typhoon1612 <119401052+Typhoon1612@users.noreply.github.com>
2025-07-29 02:01:36 +0800

    29/7/2025 0201

--- src/sections/Body.tsx
+++ src/sections/Body.tsx
@@ -3,7 +3,7 @@ import Introduction from "../components/Introduction.tsx";
 
  function Body () {
      return (
-       <div className="section-1-background flex-row place-items-center md:items-center md:flex justify-center pl-[0vh] md:pl-[10vh] pt-[20vh] md:pt-[8vh] gap-[50px] lg:gap-[150px] xl:gap-[250px] ">
+       <div className="section-1-background flex-row  md:items-center md:flex justify-center pl-[0vh] md:pl-[10vh] pt-[20vh] md:pt-[8vh] gap-[50px] lg:gap-[150px] xl:gap-[250px] ">
          <ProfilePic />
          <Introduction />
        </div>
