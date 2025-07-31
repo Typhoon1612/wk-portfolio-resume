04027fce296852f418fb3b231e5188d2933a8d32
Typhoon1612 <119401052+Typhoon1612@users.noreply.github.com>
2025-07-31 15:15:59 +0800

    31/7/2025 1515

--- src/sections/Body.tsx
+++ src/sections/Body.tsx
@@ -1,13 +1,36 @@
 import ProfilePic from "../components/ProfilePicture.tsx";
 import Introduction from "../components/Introduction.tsx";
+import Sky from "../assets/images/sky.png";
+import Sea from "../assets/images/underTheSea.png";
+import { Parallax } from "react-parallax";
 
 function Body() {
   return (
     <>
-      <div className="section-1-background flex flex-col md:flex-row items-center justify-center h-screen  pl-[0vh] md:pl-[10vh] pt-[5vh] md:pt-[8vh] gap-[50px] lg:gap-[150px] xl:gap-[250px] ">
-        <ProfilePic />
-        <Introduction />
-      </div>
+      <Parallax
+        strength={800}
+        bgImageStyle={{
+          height: "100vh",
+          width: "100%",
+          objectFit: "cover",
+          filter: "grayscale(0.2)"
+        }}
+        blur={1}
+        //bgClassName="bg-cover, bg-center, bg-no-repeat"
+        bgImage={Sky}
+      >
+        <div className="flex flex-col md:flex-row items-center justify-center h-screen pl-[0vh] md:pl-[10vh] pt-[5vh] md:pt-[8vh] gap-[50px] lg:gap-[150px] xl:gap-[250px]">
+          <ProfilePic />
+          <Introduction />
+        </div>
+      </Parallax>
+      <Parallax
+        strength={800}
+        bgImageStyle={{ height: "100vh", width: "100%", objectFit: "cover",}}
+        bgImage={Sea}
+      >
+        <div className="h-screen">My Name</div>
+      </Parallax>
     </>
   );
 }
--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,12 +1,12 @@
 import WaterButton from "../components/WaterButton";
 import wkPirateLogo from "../assets/images/wkPirateLogo.png";
-import downloadIcon from "../assets/images/downloadIcon.svg";
+import downloadIcon from "../assets/images/downloadIcon.png";
 import resume from "../assets/docs/resume.pdf";
 
 function Navbar() {
   return (
     <>
-      <nav className="fixed top-0 left-0 right-0 flex justify-between items-center px-[10px] lg:px-40 pt-5 h-auto bg-transparent z-50 min-w-50">
+      <nav className="fixed top-0 left-0 right-0 flex justify-between items-center px-[10px] lg:px-20 pt-5 h-auto bg-transparent z-50 min-w-50">
         {/* Logo */}
 
         <img src={wkPirateLogo} alt="Wk_Pirates_Logo" className="w-20 lg:w-50" />
